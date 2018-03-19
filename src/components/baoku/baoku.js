ff3e4a34a19ec4c3edefbd962362c37c32b65fb8
aoove <10226093@qq.com>
2018-03-19 15:25:56 +0800

    test3

--- src/components/baoku/baoku.js
+++ src/components/baoku/baoku.js
@@ -6,7 +6,7 @@ export default class Component extends React.Component {
   render() {
     return (
       <div className="baoku">
-        <iframe src="http://apics.baoku.com/open/api/login/oaLogin?&timestamp=20180319150757&vouchered=ce892e7d-388f-4f2e-9761-ebe96372b8a6&sign=faf809db4269ec12f5d8824ba3919b0e&passgers=41600&homePage=air&bkcid=49003&emnum=41600&direction=H5&isDisplayTW=false"></iframe>
+        <iframe src="http://phone.baoku.com/domair/airbook/toQuery?1=1&isAppCom=null&headercolor=null"></iframe>
       </div>
     );
   }
